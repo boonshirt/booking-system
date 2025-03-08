0ec2eebe72d6d86f1411a670bbc0bdc871936fed
boonshirt <92785236+boonshirt@users.noreply.github.com>
2025-03-09 00:13:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    aprUrl: "https://script.google.com/macros/s/AKfycbxVUinvWzsp178oiD7ldBJFlc3J-Bke1377PsJ9uuBIbrKgI-TlxuO0xtzmqSV3af0b/exec"
+    aprUrl: "https://script.google.com/macros/s/AKfycbzQ-9Q7ILH2rtUhrugVJIoXpkw7wdayNIeFJc88g_YnL2CIYhRCYZBwZD8L-V_2EuEyMQ/exec"
 }
